90745e1e00d3f8d047795e1b17db00954c8e856b
Chris Campbell <chrs.campbell@shopify.com>
2023-03-30 18:08:12 -0600

    setup for nixos execution

--- default.nix
+++ default.nix
@@ -1,21 +1,26 @@
+with import <nixpkgs> {};
+
 let
-    pkgs = import <nixpkgs> {};
-    starship_config=pkgs.fetchgit {
+    dotfiles=pkgs.fetchgit {
         url = "https://github.com/campbel/dotfiles.git";
         rev = "fe0930d3cbd2bcc3109765672e13f342fcb95f93";
+        sha256 = "cW5wTwkVMwjin6sjKRYA+TQgGVeVwkpFSTbvhoWez/4=";
     };
-
-    stdenv.mkDerivation {
-        name = "campbel-shell";
-        buildInputs = [
-            htop
-            jq
-            tree
-            go_1_20
-        ];
-    }
 in
-    pkgs.runCommand "starship-config" {} ''
-        mkdir -p $out/dotfiles
-        cp ${starship_config} $out/dotfiles/starship_config
-    ''
+
+stdenv.mkDerivation {
+    name = "campbel-shell";
+    buildInputs = [
+        jq
+        go_1_20
+        zsh
+        starship
+    ];
+    shellHook = ''
+        echo "Copying files from Git repository to home directory..."
+        mkdir -p ~/.config
+        cp -R ${dotfiles}/config/starship.toml ~/.config/starship.nix.toml
+        echo "Files copied successfully!"
+    '';
+}
+
