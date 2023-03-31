f89a372532a7ec40f13fd9efc7c2d718e145fba9
Chris Campbell <chrs.campbell@shopify.com>
2023-03-30 18:17:24 -0600

    fix sha

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
     dotfiles=pkgs.fetchgit {
         url = "https://github.com/campbel/dotfiles.git";
         rev = "8838ca5a5f41198039bc90a4407f008104cbf8e0";
-        sha256 = "cW5wTwkVMwjin6sjKRYA+TQgGVeVwkpFSTbvhoWez/4=";
+        sha256 = "dUD5PbCWCnPfKfXGg52yh6Y1qsjDA7JeqmYK9d1fdCc=";
     };
 in
 
